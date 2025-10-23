6b54d7670f17f0d8d99bd09ad4ccb39f5d1961fa
mmorenomor12 <mmorenomor12@educacion.navarra.es>
2025-10-23 08:44:16 +0200

    terminado a comprobar

--- src/Carrera.java
+++ src/Carrera.java
@@ -10,18 +10,24 @@ public class Carrera {
         for (int i = 0; i <= 20; i++) {
             int opcion1 = (int)(Math.random()*4)+1;
             int opcion2 = (int)(Math.random()*4)+1;
+            System.out.println("");
             System.out.println("Turno del caballo 1:");
             caballoJugador1.correr(opcion1);
+            System.out.println("");
             System.out.println("Turno del caballo 2:");
             caballoJugador2.correr(opcion2);
+            System.out.println("********************************************************");
             if(i == 20){
                 if(caballoJugador1.getDistanciaRecorrida() > caballoJugador2.getDistanciaRecorrida()){
+                    System.out.println("");
                     System.out.println("El caballo 1 ha ganado la carrera");
                     System.out.println("con " +  caballoJugador1.getDistanciaRecorrida() + " de distancia recorrida");
                 } else if(caballoJugador1.getDistanciaRecorrida() < caballoJugador2.getDistanciaRecorrida()){
+                    System.out.println("");
                     System.out.println("El caballo 2 ha ganado la carrera");
                     System.out.println("con " +  caballoJugador2.getDistanciaRecorrida() + " de distancia recorrida");
                 }else{
+                    System.out.println("");
                     System.out.println("Han empatado los caballos");
                     System.out.println("con " +  caballoJugador1.getDistanciaRecorrida() + " de distancia recorrida el caballo 1");
                     System.out.println("con " +  caballoJugador2.getDistanciaRecorrida() + " de distancia recorrida el caballo 2");
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
         Carrera carrera = new Carrera();
 
-        System.out.println("*********************************************");
+        System.out.println("********************************************************");
         System.out.println("INICIA LA CARRERA  ");
-        System.out.println("*********************************************");
+        System.out.println("********************************************************");
         carrera.iniciarCarrera();
-        System.out.println("*********************************************");
+        System.out.println("********************************************************");
         System.out.println("FIN DE LA CARRERA  ");
-        System.out.println("*********************************************");
+        System.out.println("********************************************************");
     }
 }
